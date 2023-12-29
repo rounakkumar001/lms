940be94128b52c407f79cf521f7f1bf85866cb0e
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2023-12-29 19:19:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A E-Learning Site. Using MERN.
         </p>
         <a
           className="App-link"
